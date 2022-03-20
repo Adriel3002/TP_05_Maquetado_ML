e882f78fa6583b9c338f7696e028a894340a1162
adriel3002 <adrielcoria1914@gmail.com>
2022-03-20 14:31:03 -0300

    Terminando el Maquetado de Mercado Liebre

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const path = require ('path')
+const path = require ('path');
 const app = express();
 const port = 3030;
 
-app.use (express.static('public'))
+app.use (express.static('public'));
 
-app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
 
 app.listen(port,() => console.log(`Server running in http://localhost:${port}`));
 
