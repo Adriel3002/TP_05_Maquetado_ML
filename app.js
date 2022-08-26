01473348f8daff5e738e086b7a63e4ee2f41f2fb
Adriel Coria <99737740+Adriel3002@users.noreply.github.com>
2022-08-26 08:17:40 -0300

    server funcionando

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3030;
 app.use (express.static('public'));
 
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
+/* app.get('/', (req,res) => res.send('Mercado Liebre'));
+ */
 
 app.listen(port,() => console.log(`Server running in http://localhost:${port}`));
 
